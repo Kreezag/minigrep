aa75f00a22b62474043f2a4bd613a7b36bc75e4f
Andrei Kuchuk <kreezag@gmail.com>
2024-10-10 23:44:03 +0400

    user iterator rather then clone in argguments

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use std::{env, process};
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|e| {
+    let config = Config::build(env::args()).unwrap_or_else(|e| {
         println!("Problem Parsing: {e}");
 
         process::exit(1);
